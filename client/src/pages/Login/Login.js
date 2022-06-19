29ec0ce2019150a4fb42ace8331a385f0f851ac8
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2022-06-19 11:50:35 +0700

    add animation

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -3,61 +3,63 @@ import PropTypes from "prop-types";
 import LoginForm from "./components/LoginForm";
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
-import socials from "../../config/socials";
 
+import socials from "../../config/socials";
+import languages from "../../config/languages";
 import ToggleButton from "../../components/ToggleButton";
 import Button from "../../components/Button";
-
-let languageList = [
-  {
-    code: "en",
-    name: "English",
-  },
-  { code: "vi", name: "Việt Nam" },
-];
+const transition = {
+  // type: "spring",
+  // stiffness: 500,
+  // damping: 20,
+  duration: 0.5,
+};
 
 const LoginPage = (props) => {
   const [language, setLanguage] = useState(
     localStorage.getItem("lang") || "en",
   );
   const { t, i18n } = useTranslation();
+
+  const [isLoginForm, setIsLoginForm] = useState(true);
+
   const handleChangeLanguage = (language) => {
     setLanguage(language);
     i18n.changeLanguage(language);
     localStorage.setItem("lang", language);
   };
+
   return (
-    <div className="w-full flex justify-evenly text-center">
-      <div className="absolute right-8 top-8 font-bold text-lg flex">
-        <ToggleButton />
-        {languageList.map((languageItem) => (
-          <motion.span
-            whileHover={{ scale: 1.2 }}
-            whileTap={{
-              scale: 0.8,
-            }}
-            key={languageItem.code}
-            className={`${
-              languageItem.code === language
-                ? "text-primary"
-                : "dark:text-dark-textBold"
-            } cursor-pointer ml-2`}
-            onClick={() => handleChangeLanguage(languageItem.code)}
-          >
-            <span className="uppercase">{languageItem.code}</span>
-          </motion.span>
-        ))}
-      </div>
-      <div className="flex-1 bg-primary h-full">left</div>
+    <div
+      className={`w-full flex justify-evenly text-center  ${
+        isLoginForm ? "flex-row-reverse" : ""
+      } `}
+    >
       <motion.div
-        initial={{ translateY: "-50%" }}
-        whileHover={{ scale: 1.2 }}
-        whileTap={{
-          scale: 0.8,
-        }}
-        className="absolute bg-primary top-1/2 w-[100px] h-[100px] rounded-full -translate-y-1/2 border-8 border-white dark:border-dark-veryLight cursor-pointer"
-      ></motion.div>
-      <div className="flex-1 h-full flex items-center justify-center dark:bg-dark-veryLight">
+        layout
+        transition={transition}
+        className="flex-1 h-full flex items-center relative justify-center z-90 dark:bg-dark-veryLight"
+      >
+        <div className="absolute right-8 top-8 font-bold text-lg flex">
+          <ToggleButton />
+          {languages.map((languageItem) => (
+            <motion.span
+              whileHover={{ scale: 1.2 }}
+              whileTap={{
+                scale: 0.8,
+              }}
+              key={languageItem.code}
+              className={`${
+                languageItem.code === language
+                  ? "text-primary"
+                  : "dark:text-dark-textBold"
+              } cursor-pointer ml-2`}
+              onClick={() => handleChangeLanguage(languageItem.code)}
+            >
+              <span className="uppercase">{languageItem.code}</span>
+            </motion.span>
+          ))}
+        </div>
         <div className="min-h-[46px] w-[460px]  flex flex-col gap-4">
           <div className="text-[36px] font-bold capitalize dark:text-dark-textBold">
             {t("login to")}
@@ -80,7 +82,23 @@ const LoginPage = (props) => {
           </div>
           <LoginForm />
         </div>
-      </div>
+      </motion.div>
+      <motion.div
+        initial={{ translateY: "-50%" }}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{
+          scale: 0.8,
+        }}
+        className="absolute bg-primary top-1/2 w-[100px] h-[100px] rounded-full z-90 -translate-y-1/2 border-8 border-white dark:border-dark-veryLight cursor-pointer"
+        onClick={() => setIsLoginForm((prev) => !prev)}
+      ></motion.div>
+      <motion.div
+        layout
+        transition={transition}
+        className="flex-1 bg-primary h-full"
+      >
+        left
+      </motion.div>
     </div>
   );
 };
